f8d93bb4e20f009e14312f0a877a9cccaa1d5783
fousekjan <jan.fousek@univ-amu.fr>
2021-07-29 07:56:24 +0200

    pre-presentation

--- utils.py
+++ utils.py
@@ -36,3 +36,29 @@ def generate_initial_conditions_array(conn, dt, model):
     nmodes = model.number_of_modes
 
     return np.zeros( (horizon, nvar, nnodes, nmodes) )    
+
+
+import anytree, logging, contextlib
+
+@contextlib.contextmanager
+def no_log(highest_level=logging.CRITICAL):
+    "Disable logging within context."
+    # https://gist.github.com/simon-weber/7853144
+    previous_level = logging.root.manager.disable
+    logging.disable(highest_level)
+    try:
+        yield
+    finally:
+        logging.disable(previous_level)
+
+        
+def region_select_props(atlas, roi_name):
+    "Select atlas region w/o logging, return centroid & is_cortical."
+    try:
+        with no_log():
+            atlas.select_region(roi_name)
+        props, = atlas.regionprops(bs.spaces.MNI_152_ICBM_2009C_NONLINEAR_ASYMMETRIC).values()
+        return props.centroid_mm, props.is_cortical
+    except (anytree.CountError, AttributeError) as exc:
+        print(f'skipping {roi_name}: {exc}')
+        return np.r_[0,0,0], False
